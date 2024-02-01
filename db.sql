1f872dfb730f244b677b64b2e3f262b148b7147a
Didirexha <153300036+Didirexha@users.noreply.github.com>
2024-02-01 23:52:42 +0100

    db changes

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 27, 2024 at 01:16 AM
+-- Generation Time: Feb 01, 2024 at 11:50 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -37,10 +37,31 @@ CREATE TABLE `blogs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`name`, `email`, `message`) VALUES
+('didi', 'didi@gmail.com', 'Message is sent'),
+('Ilaz', 'ilaz.gashi@gmail.com', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
+  `id` varchar(30) NOT NULL DEFAULT current_timestamp(),
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -52,11 +73,11 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`name`, `surname`, `email`, `password`, `role`) VALUES
-('admin', 'admin', 'admin', 'admin', ''),
-('[value-1]', '[value-2]', '[value-3]', '[value-4]', ''),
-('didi', 'didi', 'didi', 'didi', ''),
-('didi', 'didi', 'didi', 'didi', '');
+INSERT INTO `user` (`id`, `name`, `surname`, `email`, `password`, `role`) VALUES
+('1', 'admin', 'admin', 'admin@admin.com', 'admin', 'admin'),
+('2', 'didi', 'rexha', 'didi@gmail.com', 'didi', 'user'),
+('2024-01-31 23:59:48', 'Ilaz', 'Gashi', 'ilaz.gashi@gmail.com', 'ilazi', 'user'),
+('2024-02-01 00:39:35', 'haxhi', 'halilaj', 'haxhihalilaj@scannova.com', 'haxhia', 'user');
 
 -- --------------------------------------------------------
 
@@ -70,6 +91,16 @@ CREATE TABLE `user_messages` (
   `message` varchar(255) NOT NULL,
   `id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD KEY `id` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
